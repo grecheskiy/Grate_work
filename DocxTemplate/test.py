753871d4c510f02f7ed47fc5061e045baef2fa66
Maxim Grekov <grecheskiy@rambler.ru>
2024-05-08 12:50:11 +0300

    changeN

--- DocxTemplate/test.py
+++ DocxTemplate/test.py
@@ -1,21 +1,38 @@
 from num2words import num2words
 import math
+#
+# num = float(input("Enter number: "))
+# n = math.trunc(num)
+# # print(n)
+#
+#
+# def numwords():
+#     return num2words(n, lang='ru')
+#
+#
+# number = str(numwords())
+# # print(number)
+#
+#
+# if "." in str(num):
+#     cop = str(num).split(".")[-1]
+#     # print(nn)
+#
+#     print(f"{number} рублей {cop} копеек")
 
-num = float(input("Enter number: "))
-n = math.trunc(num)
-# print(n)
 
+def numerWords():
+    cop2 = 0
+    number = float(input())
+    n = math.trunc(number)
+    number_text = str(num2words(n, lang='ru'))
+    if "." in str(number):
+        cop = str(number).split(".")[-1]
+        if "0" in cop:
+            cop2 = cop
+        else:
+            cop2 = cop + "0"
+    result = f"{number_text} рублей {cop2} копеек"
+    return result
 
-def numwords():
-    return num2words(n, lang='ru')
-
-
-number = str(numwords())
-# print(number)
-
-
-if "." in str(num):
-    cop = str(num).split(".")[-1]
-    # print(nn)
-
-    print(f"{number} рублей {cop} копеек")
+print(numerWords())
